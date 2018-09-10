019c4f09028cbbfe4e34e79e4c478ddacd2b7eec
BrendanJones44 <bpj1651@rit.edu>
2018-09-09 22:00:53 -0400

    background colors switching

--- static/color_picker.js
+++ static/color_picker.js
@@ -1,18 +1,48 @@
 colorPickerShowing = false;
 colorPickerId = "color-picker";
+selectedTR = null;
+selectedTD = null;
 
 function buttonNumber(buttonString) {
   return buttonString.replace("color-btn-", "")
 }
 
+function setTableDark(num){
+  for(i=0; i < 5; i++){
+    if (i !== Number(num)){
+      document.getElementById("tr-" + i).style.background = "#7b7d79";
+      document.getElementById("td-" + i).style.background = "#7b7d79";
+    }
+  }
+}
+
+function setTableLight(){
+  for (i = 0; i < 5; i++) {
+    document.getElementById("tr-" + i).style.background = "#eeeeee";
+    document.getElementById("td-" + i).style.background = "#eeeeee";
+  }
+}
+
 function handleColorInputClick() {
+  buttonNum = buttonNumber(this.id);
   colorPicker = document.getElementById(colorPickerId);
-  colorPickerId = "color-picker-" + buttonNumber(this.id);
+  colorPickerId = "color-picker-" + buttonNum;
   colorPicker.id = colorPickerId;
+  body = document.getElementsByTagName("BODY")[0];
+  tr = document.getElementById("tr-" + buttonNum);
+  td = document.getElementById("td-" + buttonNum);
   if (colorPickerShowing) {
     colorPicker.style.visibility = "hidden";
+    body.style.background = "#eeeeee";
+    setTableLight();
   } else {
     colorPicker.style.visibility = "visible";
+    body.style.background = "#7b7d79";
+    tr.style.background = "#e9e9e9";
+    td.style.background = "#e9e9e9";
+    setTableDark(buttonNum);
+    selectedTR = tr;
+    selectedTD = td;
   }
   colorPickerShowing = !colorPickerShowing;
 }
